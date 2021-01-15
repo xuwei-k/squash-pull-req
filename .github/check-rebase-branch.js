ffd9f2b095ec6356a5bf0581fecf330cc1c3db82
xuwei-k <6b656e6a69@gmail.com>
2021-01-15 16:18:01 +0900

    b

--- .github/check-rebase-branch.js
+++ .github/check-rebase-branch.js
@@ -1,15 +1,13 @@
 module.exports = ({github, context}) => {
   (async () => {
-    console.log(github);
-    console.log(context);
     const pull_req = await github.pulls.get({
       owner: context.repo.owner,
       repo: context.repo.repo,
       pull_number: context.payload.issue.number
     });
     console.log(pull_req);
-    console.log(pull_req.head);
-    console.log(pull_req.head.ref);
+    console.log(pull_req.data.head);
+    console.log(pull_req.data.head.ref);
     if (pull_req.head.ref === "ppp") {
       throw "このbranchはrebase禁止";
     }
